dc5abef6d6897b546a23677e93849969c7d15993
jonas18121 <jonasxtrm971@gmail.com>
2024-04-21 17:54:42 +0200

    [test] : form contact

---
+++ app/src/DTO/ContactDTO.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ContactDTO
+{
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 254)]
+    public string $name;
+
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    public string $email;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 2500)]
+    public string $message; 
+}
---
+++ app/src/Form/ContactType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\DTO\ContactDTO;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'empty_data' => 'Non Rempli'
+            ])
+            ->add('email', EmailType::class, [
+                'empty_data' => 'Non Rempli'
+            ])
+            ->add('message', TextareaType::class, [
+                'empty_data' => 'Non Rempli'
+            ])
+            ->add('Enregistrer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContactDTO::class,
+        ]);
+    }
+}
